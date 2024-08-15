da08b977a9b5b647fbac7348dff2601c5a06e8fc
luizhenriqque18 <luizhenriqque15@gmail.com>
2024-08-15 16:15:34 -0400

    Start project with springboot

---
+++ src/main/java/io/github/luizhenriqque18/order_ws/OrderWsApplication.java
@@ -0,0 +1,13 @@
+package io.github.luizhenriqque18.order_ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderWsApplication.class, args);
+	}
+
+}
